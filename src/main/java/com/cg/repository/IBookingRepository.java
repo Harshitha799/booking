4a53b354d327daee0f4d0d22adc96733d0a83179
Harshitha799 <80540379+Harshitha799@users.noreply.github.com>
2021-04-27 00:11:19 +0530

    Update IBookingRepository.java

--- src/main/java/com/cg/repository/IBookingRepository.java
+++ src/main/java/com/cg/repository/IBookingRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.cg.dto.Bookingdto9;
-import com.cg.entity.Booking9;
-import com.cg.entity.FeedBack9;
+import com.cg.dto.Bookingdto;
+import com.cg.entity.Booking;
+import com.cg.entity.FeedBack;
 
 /******************************************************************
  * 
@@ -21,39 +21,23 @@ import com.cg.entity.FeedBack9;
  ******************************************************************/
 
 @Repository
-public interface IBookingRepository extends JpaRepository<Booking9, Long>
+public interface IBookingRepository extends JpaRepository<Booking, Long>
 {
 
-	public Booking9 findAllByBookingId(long bookingId);
+	public Booking findAllByBookingId(long bookingId);
 
-	public Bookingdto9 save(Bookingdto9 bookingdto);
+	public Bookingdto save(Bookingdto bookingdto);
 
 	boolean existsByBookingId(long bookingId);
 
-	@Query("select b from Booking9 b , User9 u where b.username=u.username and u.username=:username")
-	public Booking9 findAllByUsername(@Param("username") String username);
+	@Query("select b from Booking b , User u where b.username=u.username and u.username=:username")
+	public Booking findAllByUsername(@Param("username") String username);
 		
-	@Query("select fb from FeedBack9 fb where fb.routeName = :routeName  ")
-	public List<FeedBack9> getFeedBackByBusRoute(@Param("routeName") String routeName);
+	@Query("select fb from FeedBack fb where fb.routeName = :routeName  ")
+	public List<FeedBack> getFeedBackByBusRoute(@Param("routeName") String routeName);
 
 
 
 	
 
-	
-
-
-	
-
-	
-	
-	
-	
-	
-
-	//public Booking9 getBookingByBookingId(long bookingId);
-
-
-
-	
 }
